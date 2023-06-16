9d6fd276f0b78b1bbae2fc9e9330bfb0c8b71306
Brenden Olding <oldingb21@gmail.com>
2023-06-16 03:05:51 -0400

    added variables and modified HTML elements

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('JS file connected')
+// Important Variables 
+
+const eastern = document.querySelector('#eastern');
+const western = document.querySelector('#western');
+const atlantic = document.querySelector('#atlantic_list');
+const metro = document.querySelector('#metropolitan_list');
+const central = document.querySelector('#central_list');
+const pacific = document.querySelector('#pacific_list');
+const past = document.querySelector('#past_list')
+
+// Callback Functions
