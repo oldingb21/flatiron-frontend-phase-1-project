f51aa140b34b72d83191a9636d6dcc2b0c1f8ffa
Brenden Olding <oldingb21@gmail.com>
2023-06-20 01:34:19 -0400

    created my fetch for active teams and fixed some issues in callbacks, removed some CSS causing issues

--- index.js
+++ index.js
@@ -10,17 +10,21 @@ const pastTeams = document.querySelector('#past_list');
 const current = document.querySelector('#current')
 const past = document.querySelector('#past')
 const divisions = document.querySelectorAll('.division');
+const activeBtn = document.querySelector('#active_btn');
+const inactiveBtn = document.querySelector('#inactive_btn')
 
 
 
 // Callback Functions
-
-const createTeamElements = team => {
+// I have to create multiple callbacks to do similar things, because
+// I have to use 2 APIs in order to get all the data I want, and the paths
+// are not the same in each API
+const createActiveTeam = team => {
     //create elements to display team details
     const listElement = document.createElement('li')
     const teamName = document.createElement('h4');
     teamName.classList = 'team_name'
-    teamName.textContent = team.teamName;
+    teamName.textContent = team.name;
     const teamElements = document.createElement('ul');
     const teamVenue = document.createElement('li');
     teamVenue.textContent = `Venue: ${team.venue.name}`;
@@ -32,7 +36,7 @@ const createTeamElements = team => {
     teamWebsite.href = team.officialSiteUrl;
     teamWebsite.textContent = team.officialSiteUrl;
     listElement.append(teamName, teamElements);
-    teamElements.append(teamVenue, teamCity, firstSeason, teamWebsite);
+    teamElements.append(teamCity, teamVenue, firstSeason, teamWebsite);
     switch (team.division.name) {
         case 'Atlantic': 
             atlantic.append(listElement);
@@ -48,15 +52,34 @@ const createTeamElements = team => {
     }
 }
 
-const displayActiveTeams = (teams) => {
-    teams.forEach(createTeamElements(team))
-    eastern.removeAttribute('.hidden');
-    western.removeAttribute('.hidden');
-    atlantic.removeAttribute('.hidden');
-    metro.removeAttribute('.hidden');
-    central.removeAttribute('.hidden');
-    pacific.removeAttribute('.hidden');
-    divisions.removeAttribute('.hidden');
+const displayActiveTeams = teams => {
+    return teams.forEach(team => createActiveTeam(team))
+}
+
+const activeTeamFetch = e => {
+    fetch('https://statsapi.web.nhl.com/api/v1/teams')
+    .then(res => res.json())
+    .then(teamsData => {
+        // I have to use dot notation in the callback below
+        // because there are 2 parent objects in the returned json
+        // response. One has a copyright disclaimer and the teams below it.
+        //teamsData.teams returns an Array of teams
+        return displayActiveTeams(teamsData.teams);
+    })
+    // remove event listener to prevent the event from populating
+    // the DOM with duplicate elements
+    activeBtn.removeEventListener('click', activeTeamFetch, true);
+}
+
+const createPastTeam = team => {
+
 }
 
 // Events
+
+activeBtn.addEventListener('click', activeTeamFetch, true);
+//activeBtn.removeEventListener('click', activeTeamFetch, true);
+
+inactiveBtn.addEventListener('click', e => {
+    console.log('inactive btn event also works!')
+})
