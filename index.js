80298a5aaaf37a124ef3e3672e09c2d153be90a1
Brenden Olding <oldingb21@gmail.com>
2023-06-19 20:27:45 -0400

    changed HTML elements, wrote first chunk of callback for team display

--- index.js
+++ index.js
@@ -6,6 +6,32 @@ const atlantic = document.querySelector('#atlantic_list');
 const metro = document.querySelector('#metropolitan_list');
 const central = document.querySelector('#central_list');
 const pacific = document.querySelector('#pacific_list');
-const past = document.querySelector('#past_list')
+const pastTeams = document.querySelector('#past_list');
+const current = document.querySelector('#current')
+const past = document.querySelector('#past')
+
 
 // Callback Functions
+
+const displayActiveTeams = (teams) => {
+    teams.forEach(team => {
+        //create elemts to display team details
+        const listElement = document.createElement('li')
+        const teamName = document.createElement('h4');
+        teamName.classList = 'team_name'
+        teamName.textContent = team.teamName;
+        const teamElements = document.createElement('ul');
+        const teamVenue = document.createElement('li');
+        teamVenue.textContent = `Venue: ${team.venue.name}`;
+        const teamCity = document.createElement('li');
+        teamCity.textContent = `City: ${team.venue.city}`;
+        const firstSeason = document.createElement('li');
+        firstSeason.textContent = `First Season: ${team.firstYearOfPlay}`;
+        const teamWebsite = document.createElement('link');
+        teamWebsite.href = team.officialSiteUrl;
+        teamWebsite.textContent = team.officialSiteUrl;
+        listElement.append(teamName, teamElements);
+        teamElements.append(teamVenue, teamCity, firstSeason, teamWebsite);
+
+    })
+}
